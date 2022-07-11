5c7beff91ed21c8bfb5583efa8e205a2a9f6a870
Joaquin Esteban Bueno <buenojoaa@gmail.com>
2022-07-11 18:21:02 -0300

    hard & soft skills

---
+++ ProyectoFinal_JB/src/app/components/hardskill/hardskill.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HardskillComponent } from './hardskill.component';
+
+describe('HardskillComponent', () => {
+  let component: HardskillComponent;
+  let fixture: ComponentFixture<HardskillComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HardskillComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HardskillComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
