2ffe10299fdb8b50a58b104f6b92ae44a6bcdbe3
Joaquin Esteban Bueno <buenojoaa@gmail.com>
2022-07-11 20:10:08 -0300

    Componente Proyectos

--- ProyectoFinal_JB/src/app/app.module.ts
+++ ProyectoFinal_JB/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HardskillComponent } from './components/hardskill/hardskill.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { HardskillComponent } from './components/hardskill/hardskill.component';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HardskillComponent
+    HardskillComponent,
+    ProyectoComponent
   ],
   imports: [
     BrowserModule,
