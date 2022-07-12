1dc817e5b92e42ad47f25da1144acfa1c4d26f42
Joaquin Esteban Bueno <buenojoaa@gmail.com>
2022-07-12 16:59:15 -0300

    Se creo carpeta BackEnd

---
+++ ProyectoFinal_JB/BackEnd/src/test/java/com/porfolio/joaquinbueno/JoaquinbuenoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfolio.joaquinbueno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoaquinbuenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
