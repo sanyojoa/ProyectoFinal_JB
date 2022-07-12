9528528a874bd6c80738bc01f2022b5bc7013fb2
Joaquin Esteban Bueno <buenojoaa@gmail.com>
2022-07-12 15:04:15 -0300

    footer y css para hardskill

--- ProyectoFinal_JB/src/app/app.module.ts
+++ ProyectoFinal_JB/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HardskillComponent } from './components/hardskill/hardskill.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { ProyectoComponent } from './components/proyecto/proyecto.component';
     ExperienciaComponent,
     EducacionComponent,
     HardskillComponent,
-    ProyectoComponent
+    ProyectoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
