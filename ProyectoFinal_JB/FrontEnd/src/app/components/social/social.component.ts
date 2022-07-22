45e74de86962b454fd1c7f604c2af89abfd1dc72
Joaquin Esteban Bueno <buenojoaa@gmail.com>
2022-07-22 17:11:54 -0300

    Agregamos Login

--- ProyectoFinal_JB/FrontEnd/src/app/components/social/social.component.ts
+++ ProyectoFinal_JB/FrontEnd/src/app/components/social/social.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-social',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SocialComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-
+ login(){
+  this.router.navigate(['/login'])
+ }
 }
