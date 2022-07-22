cd86f2dcce053884a5e1de18a8e2e56f86fc90fe
Joaquin Esteban Bueno <buenojoaa@gmail.com>
2022-07-22 16:40:22 -0300

    Aplicamos rooteo para login y home

--- ProyectoFinal_JB/FrontEnd/src/app/app.module.ts
+++ ProyectoFinal_JB/FrontEnd/src/app/app.module.ts
@@ -15,6 +15,8 @@ import { HardskillComponent } from './components/hardskill/hardskill.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
 import {HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +30,9 @@ import {HttpClientModule} from '@angular/common/http';
     EducacionComponent,
     HardskillComponent,
     ProyectoComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
