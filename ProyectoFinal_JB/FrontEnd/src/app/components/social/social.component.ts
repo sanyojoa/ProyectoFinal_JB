1c17432497c98c2837214f2e57c6d318f739630c
Joaquin Esteban Bueno <buenojoaa@gmail.com>
2022-08-02 16:01:55 -0300

    Update social- button login (Ingresar/salir)

--- ProyectoFinal_JB/FrontEnd/src/app/components/social/social.component.ts
+++ ProyectoFinal_JB/FrontEnd/src/app/components/social/social.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-social',
@@ -7,11 +8,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./social.component.css']
 })
 export class SocialComponent implements OnInit {
+  isLoged = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private tokenService: TokenService) { }
 
   ngOnInit(): void {
+    if(this.tokenService.getToken()){
+      this.isLoged = true;
+    }else{
+      this.isLoged = false;
+    }
   }
+
+  onLogOut():void{
+    this.tokenService.logOut();
+    window.location.reload();
+  }
+
  login(){
   this.router.navigate(['/login'])
  }
