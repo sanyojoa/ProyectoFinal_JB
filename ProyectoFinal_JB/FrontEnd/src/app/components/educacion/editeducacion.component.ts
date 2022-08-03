b9453a31743ad64e3214a459c3136054b46e81b8
Joaquin Esteban Bueno <buenojoaa@gmail.com>
2022-08-03 18:07:06 -0300

    Update editeducacion.component.ts

--- ProyectoFinal_JB/FrontEnd/src/app/components/educacion/editeducacion.component.ts
+++ ProyectoFinal_JB/FrontEnd/src/app/components/educacion/editeducacion.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Educacion } from 'src/app/model/educacion';
+import { EducacionService } from 'src/app/service/educacion.service';
 
 @Component({
   selector: 'app-editeducacion',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editeducacion.component.css']
 })
 export class EditeducacionComponent implements OnInit {
+  educacion: Educacion = null;
 
-  constructor() { }
+  constructor(private educacionS: EducacionService, private activatedRouter: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    const id = this.activatedRouter.snapshot.params['id'];
+    this.educacionS.detail(id).subscribe(
+      data =>{
+        this.educacion = data;
+      }, err =>{
+        alert("Error al modificar");
+        this.router.navigate(['']);
+      }
+    )
   }
 
+  onUpdate(): void{
+    const id = this.activatedRouter.snapshot.params['id'];
+    this.educacionS.update(id, this.educacion).subscribe(
+      data =>{
+        this.router.navigate(['']);
+      }, err =>{
+        alert("Error al modificar la Educación");
+        this.router.navigate(['']);
+      }
+    )
+  }
 }
