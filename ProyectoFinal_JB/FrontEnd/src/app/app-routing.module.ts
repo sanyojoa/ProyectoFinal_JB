ee58fa4b9a992b78e14b93505719a38dc4a663fa
Joaquin Esteban Bueno <buenojoaa@gmail.com>
2022-08-03 17:46:13 -0300

    Agregamos ruta new educacion en app-routing.module

--- ProyectoFinal_JB/FrontEnd/src/app/app-routing.module.ts
+++ ProyectoFinal_JB/FrontEnd/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NeweducacionComponent } from './components/educacion/neweducacion.component';
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { HomeComponent } from './components/home/home.component';
@@ -9,7 +10,8 @@ const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'login', component: LoginComponent},
   {path: 'nuevaexp', component: NewExperienciaComponent},
-  {path: 'editexp/:id', component: EditExperienciaComponent}
+  {path: 'editexp/:id', component: EditExperienciaComponent},
+  {path: 'nuevaedu' , component: NeweducacionComponent}
 
 ];
 
