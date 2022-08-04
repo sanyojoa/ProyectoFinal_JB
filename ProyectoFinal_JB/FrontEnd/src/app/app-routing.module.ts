d6d8590827e5c4a8870f5e8d633988516545020a
Joaquin Esteban Bueno <buenojoaa@gmail.com>
2022-08-04 20:17:59 -0300

    Se creo new/edit proyecto.component

--- ProyectoFinal_JB/FrontEnd/src/app/app-routing.module.ts
+++ ProyectoFinal_JB/FrontEnd/src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { EditExperienciaComponent } from './components/experiencia/edit-experien
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { EditproyectoComponent } from './components/proyecto/editproyecto.component';
+import { NewproyectoComponent } from './components/proyecto/newproyecto.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
@@ -13,7 +15,9 @@ const routes: Routes = [
   {path: 'nuevaexp', component: NewExperienciaComponent},
   {path: 'editexp/:id', component: EditExperienciaComponent},
   {path: 'nuevaedu' , component: NeweducacionComponent},
-  {path: 'editedu/:id', component: EditeducacionComponent}
+  {path: 'editedu/:id', component: EditeducacionComponent},
+  {path: 'nuevopro', component: NewproyectoComponent},
+  {path: 'editpro/:id', component: EditproyectoComponent}
 
 ];
 
--- ProyectoFinal_JB/FrontEnd/src/app/components/proyecto/proyecto.component.ts
+++ ProyectoFinal_JB/FrontEnd/src/app/components/proyecto/proyecto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Proyecto } from 'src/app/model/proyecto';
+import { ProyectoService } from 'src/app/service/proyecto.service';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-proyecto',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./proyecto.component.css']
 })
 export class ProyectoComponent implements OnInit {
-
-  constructor() { }
+  proyecto: Proyecto[] = [];
+  
+  constructor(private proyectoS: ProyectoService, private tokenService: TokenService) { }
+  isLoged = false;
 
   ngOnInit(): void {
+    this.cargarProyecto();
+    if(this.tokenService.getToken()){
+      this.isLoged = true;
+    }else{
+      this.isLoged = false;
+    }
   }
 
+  cargarProyecto():void{
+    this.proyectoS.lista().subscribe(
+      data =>{
+        this.proyecto = data;
+      }
+    )
+  }
+
+  borrar(id?: number){
+    if(id != undefined){
+      this.proyectoS.delete(id).subscribe(
+        data =>{
+          this.cargarProyecto();
+        }, err =>{
+          alert("No se pudo eliminar");
+        }
+      )
+    }
+  }
 }
